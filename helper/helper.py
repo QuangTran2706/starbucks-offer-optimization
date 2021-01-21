55981e61b6c6a3b1b29bf61565fff6ad98456694
QuangTran2706 <s3757281@rmit.edu.vn>
2021-01-21 10:50:30 +0700

    add api and dashboard

---
+++ helper/helper.py
@@ -0,0 +1,123 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, accuracy_score, classification_report, roc_auc_score, roc_curve, confusion_matrix, auc
+import pickle
+import pandas as pd
+import numpy as np
+
+best_model = None
+df = pd.read_csv("./data/new_data.csv", sep=',', decimal='.')
+portfolio = pd.read_csv("./data/portfolio_new.csv", sep=',', decimal='.')
+
+def getData(dataFrame):
+    X = dataFrame.drop(['customer_id', 'amount', 'event_offer completed', 'event_offer received', 'event_offer viewed', 'event_transaction', 'offer_succeed'], axis=1)
+    Y = dataFrame['offer_succeed']
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    return [X_train, X_test, y_train, y_test]
+
+def evaluate_model_performance(clf, X_train, y_train):  
+    y_pred_rf = clf.predict(X_train)
+    clf_accuracy = accuracy_score(y_train, y_pred_rf)
+    clf_f1_score = f1_score(y_train, y_pred_rf)
+
+    return clf_accuracy, clf_f1_score
+
+def loadModels():
+    adaClf_filename = "model/adaClf_model.pkl"
+    logRegression_filename = "model/logRegression_model.pkl"
+    lgbmClassifier = "model/lgbmClf_model.pkl"
+    list_models_name = [adaClf_filename, logRegression_filename, lgbmClassifier]
+    models = [{'name' : 'AdaBoostClassifier'}, {'name' : 'LogisticRegression'}, {'name' : 'LGBMClassifier'}]
+    for n, i in enumerate(list_models_name):
+        with open(i, 'rb') as file:
+            models[n]['model'] = pickle.load(file)
+    return models
+
+def loadModels():
+    adaClf_filename = "model/adaClf_model.pkl"
+    logRegression_filename = "model/logRegression_model.pkl"
+    lgbmClassifier = "model/lgbmClf_model.pkl"
+    list_models_name = [adaClf_filename, logRegression_filename, lgbmClassifier]
+    models = [{'name' : 'AdaBoostClassifier'}, {'name' : 'LogisticRegression'}, {'name' : 'LGBMClassifier'}]
+    for n, i in enumerate(list_models_name):
+        with open(i, 'rb') as file:
+            models[n]['model'] = pickle.load(file)
+    return models
+
+    
+def loadModel(model_name):
+    if model_name == 'adaClf':
+        filename = "model/adaClf_model.pkl"
+    elif model_name == 'logReg':
+        filename = "model/logRegression_model.pkl"
+    elif model_name == 'lgbmClf':
+        filename = "model/lgbmClf_model.pkl"
+    else:
+        filename = "model/lgbmClf_model.pkl"
+
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+
+def getModelScore(models, X_test, y_test):
+    model_scores = []
+    for item in models:
+        model_name = item['name']
+        acc , f1 = evaluate_model_performance(item['model'],  X_test, y_test)
+        model_scores.append([model_name, acc, f1])
+    return model_scores
+
+def predict_offer_success(customer_id, offer_id, clf_model, time=0.0, amount_reward=0.0):
+    """
+    This function using model to predict whether the offer type apply to this type of customer is effective or not
+    """
+    # create customer profile
+    customer_profile = df.drop(['offer_reward', 'difficulty', 'duration', 'offer id', 'channel_email', 'channel_mobile', 'channel_social', 'channel_web', 
+                           'offer_type_bogo', 'offer_type_discount', 'offer_type_informational',  'amount', 'event_offer completed', 
+                           'event_offer received', 'event_offer viewed', 'event_transaction', 'offer_succeed', 'time', 'amount_rewarded'], axis=1)
+    customer_profile = customer_profile.drop_duplicates()
+    # Create offer profile
+    offers = portfolio.copy()
+    offers.rename(columns={'reward': 'offer_reward',
+                       'email': 'channel_email',
+                       'mobile': 'channel_mobile',
+                       'social': 'channel_social',
+                       'web': 'channel_web'}, inplace=True)
+    
+    offer = offers[offers['offer id'] == offer_id].reset_index()
+    cust = customer_profile[customer_profile['customer_id'] == customer_id].reset_index()
+    
+    # make featres
+    feature = pd.concat([cust, offer], axis=1)
+    feature.drop(["customer_id"], axis=1)
+    feature['time'] = time
+    feature['amount_rewarded'] = amount_reward
+    # predict
+    list_features = ['time', 'offer id', 'amount_rewarded', 'offer_reward', 'difficulty', 'duration',
+                 'channel_email', 'channel_mobile', 'channel_social', 'channel_web', 'offer_type_bogo', 'offer_type_discount',
+                'offer_type_informational', 'age', 'income', 'membership_duration', 'gender_F', 'gender_M', 'gender_O']
+    success = clf_model.predict(feature[list_features])
+    result = False
+    if success[0] == 0:
+        print('This type of offer is not effective against the current customer')
+    else:
+        print("offer is effective")
+        result = True
+    return result
+
+def get_evaluation_report(x_test, y_test, model):
+    y_pred = model.predict(x_test)
+    return classification_report(y_test, y_pred, output_dict=True)
+
+def get_predict_result(x_test, model):
+    y_pred = model.predict(x_test)
+    return y_pred
+
+def get_roc_curve(y_true, y_prob):
+    fpr, tpr, thresholds = roc_curve(y_true, y_prob, pos_label =True)
+    sum_ss = tpr + (1-fpr) # sum of sum_sensitivity and specificity
+    best_threshold_id = np.argmax(sum_ss)
+    best_fpr = fpr[best_threshold_id]
+    best_tpr = tpr[best_threshold_id]
+    auc_train = auc(fpr, tpr)
+    return fpr, tpr, thresholds, auc_train, best_fpr, best_tpr
---
+++ server.py
@@ -0,0 +1,198 @@
+# import pandas
+from flask import Flask, redirect, request, jsonify
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import plotly.express as px
+from plotly.subplots import make_subplots
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
+from helper.helper import *
+import pickle
+
+server = Flask(__name__)
+app = dash.Dash(__name__, server=server, url_base_pathname='/dashboard/')
+
+X_train, X_test, y_train, y_test = getData(df)
+with open("model/lgbmClf_model.pkl", 'rb') as file:
+    best_model = pickle.load(file)
+
+def create_app():
+    corr_fig = get_corr_fig(df)
+    # scatter_fig = get_scatter_figure(df)
+    roc_curves_fig = get_roc_figure()
+    model_score_fig = get_model_score_figure()
+    column_list = [i for i in list(df) if i not in ('customer_id', 'offer id')]
+    app.layout = html.Div(
+        children=[
+            html.Div(
+            className='row',
+            # Define the row element
+            children=[
+                # Define the left element
+                html.Div(className='four columns div-user-controls',
+                children = [
+                    html.H1('COSC2789 -Practical Data Science Assignment 3: Group Project'),
+                    html.H1('STARBUCKS OFFER PERSONALIZATION'),
+                    html.Div(className='div-plot-1-control',
+                        children=[html.P('''1. Correlation Matrix Graph.''')]),
+                    html.Div(className='div-plot-2-control',
+                        children=[html.P('''2. Scatter Graph.'''),
+                        html.Div(className='div-for-dropdown',
+                            children=
+                            [
+                                dcc.Dropdown(id='selector_x',
+                                options=[{"label": i, "value": i} for i in column_list],
+                                multi=False,
+                                placeholder="Select x column",)
+                            ]),      
+                        html.Div(className='div-for-dropdown',
+                            children=
+                            [
+                                dcc.Dropdown(id='selector_y',
+                                options=[{"label": i, "value": i} for i in column_list],
+                                multi=False,
+                                placeholder="Select y column",)
+                            ]),                   
+                        ]),
+                    html.Div(className='div-plot-2-control',
+                        children=[html.P('''3. ROC Curves Graph.''')]),
+                    html.Div(className='div-plot-2-control',
+                        children=[html.P('''4. Model Score Comparasion Graph.''')]),
+                ]),
+                # Define the right element
+                html.Div(className='eight columns div-for-charts bg-grey',
+                children = [
+                    dcc.Graph(id='heatmap_graph', figure=corr_fig),
+                    dcc.Graph(id='scatter_graph'),
+                    dcc.Graph(id="roc_train_graph", figure=roc_curves_fig),
+                    dcc.Graph(id='model_score_graph', figure= model_score_fig)
+                    ]),
+            ])
+        ]
+    )
+def get_corr_fig(df):
+    fig = px.imshow(df.corr(),labels=dict(color='offer_succeed'))
+    fig.update_layout({
+            'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+            'paper_bgcolor':'rgba(0, 0, 0, 0)',
+            'title': {'text': '1. Correlation Heatmap', 'font': {'color': 'white', 'size': 18}, 'x': 0.5},
+            'template': 'plotly_dark',
+            })
+    return fig
+def get_scatter_figure(df):
+    fig = px.scatter(df, x = 'income', y = 'difficulty', color='offer_succeed')
+    fig.update_layout({
+            'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+            'paper_bgcolor':'rgba(0, 0, 0, 0)',
+            'title': {'text': '2. Scatter Plot base on offer_succeed', 'font': {'color': 'white', 'size': 18}, 'x': 0.5},
+            'template': 'plotly_dark',
+            })
+    return fig
+
+def get_model_score_figure():
+    models = loadModels()
+    model_scores = getModelScore(models, X_test, y_test)
+    model_score_df = pd.DataFrame(model_scores, columns =['model', 'accuracy', 'f1'])
+    fig = go.Figure()
+    fig.add_trace(go.Bar(
+                    x=model_score_df['model'],
+                    y=model_score_df['accuracy'],
+                    name='Accuracy'))
+    fig.add_trace(go.Scatter(
+                    x=model_score_df['model'],
+                    y=model_score_df['f1'],
+                    name='F1'))
+    fig.update_layout({
+            'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+            'paper_bgcolor':'rgba(0, 0, 0, 0)',
+            'title': {'text': '4. Model Score Comparasion Graph', 'font': {'color': 'white', 'size': 18}, 'x': 0.5},
+            'template': 'plotly_dark',
+            })
+    return fig
+
+def get_roc_figure():
+    y_test_pred_proba = best_model.predict_proba(X_test)[::,1]
+    y_train_pred_proba = best_model.predict_proba(X_train)[::,1]
+
+    fpr_train, tpr_train, _, auc_train, best_fpr_train, best_tpr_train = get_roc_curve(y_train, y_train_pred_proba)
+    fpr_test, tpr_test, _, auc_test, best_fpr_test, best_tpr_test = get_roc_curve(y_test, y_test_pred_proba)
+
+    fig = make_subplots(rows=1, cols=2,
+        subplot_titles=('ROC Curve Train'))
+
+    fig.add_trace(
+        go.Scatter(x=fpr_train, y=tpr_train, name='train_roc_curve'),
+        row=1, col=1
+    )
+    fig.add_trace(go.Scatter(x=[0,1], y=[0,1],
+                    mode='lines',
+                    line=dict(color='firebrick', width=4,
+                        dash='dash')),row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=[best_fpr_train], y=[best_tpr_train],
+                    mode='markers+text',
+                    textposition="bottom right",),row=1, col=1)
+
+    fig.add_trace(
+        go.Scatter(x=fpr_test, y=tpr_test, name='test_roc_curve'),
+        row=1, col=2
+    )
+    fig.add_trace(go.Scatter(x=[0,1], y=[0,1],
+                    mode='lines',
+                    line=dict(color='firebrick', width=4,
+                              dash='dash')),row=1, col=2)
+    fig.add_trace(go.Scatter(x=[best_fpr_test], y=[best_tpr_test],
+                    mode='markers+text',
+                    textposition="bottom right",),row=1, col=2)
+
+    # Update xaxis properties
+    fig.update_xaxes(title_text="False Positive Rate", row=1, col=1)
+    fig.update_xaxes(title_text="False Positive Rate", row=1, col=2)
+
+    # Update yaxis properties
+    fig.update_yaxes(title_text="True Positive Rate", row=1, col=1)
+    fig.update_yaxes(title_text="True Positive Rate", row=1, col=2)
+    fig.update_traces(showlegend=False)
+    fig.update_layout({
+                    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+                    'paper_bgcolor':'rgba(0, 0, 0, 0)',
+                    'title': {'text': '3. ROC Curves Graph', 'font': {'color': 'white', 'size': 18}, 'x': 0.5},
+                    'template': 'plotly_dark',
+                    })
+    return fig
+# Callback for interactive scatterplot
+@app.callback(Output('scatter_graph', 'figure'),
+              [Input('selector_x', 'value'), Input('selector_y', 'value')])    
+def update_scatterplot(selector_x, selector_y):
+    fig = px.scatter(df, x=selector_x, y=selector_y, color='offer_succeed')
+    fig.update_layout({
+            'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+            'paper_bgcolor':'rgba(0, 0, 0, 0)',
+            'title': {'text': '2. Scatter Plot base on offer_succeed', 'font': {'color': 'white', 'size': 18}, 'x': 0.5},
+            'template': 'plotly_dark',
+            })
+    return fig    
+
+# Route API
+@server.route('/')
+def show_dashboard():
+    return redirect('/dashboard')
+
+@server.route('/api/evaluate/<model_name>', methods=['GET', 'POST'])
+def evaluate_model(model_name):
+    model = loadModel(model_name)
+    result = get_evaluation_report(X_test, y_test, model)
+    return jsonify({"evaluate_result": result})
+
+@server.route('/api/predict/<model_name>', methods=['GET', 'POST'])
+def predict(model_name):
+    model = loadModel(model_name)
+    result = get_predict_result(X_test, model)
+    return jsonify({"predict_result": list(result)})
+
+# Run the app
+if __name__ == '__main__':
+    create_app()
+    app.run_server(debug=True, dev_tools_ui=False, dev_tools_props_check=True)
